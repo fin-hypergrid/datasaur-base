c0df02e678d24a844c59b56d8039aac7c0a8f4a6
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2018-03-29 16:24:08 -0400

    added addListener support

--- index.js
+++ index.js
@@ -23,39 +23,27 @@ DataSourceBase.prototype = {
     initialize: function(nextDataSource, options) {
         if (nextDataSource) {
             this.next = nextDataSource;
+            this.handlers = nextDataSource.handlers;
+        } else {
+            this.handlers = [];
         }
 
         this.install(Object.getPrototypeOf(this));
     },
 
     /**
-     * @implements dataModelAPI#needs
-     * @see {@link https://fin-hypergrid.github.io/core/doc/dataModelAPI.html#needs|needs}
-     */
-    needs: function(key) {
-        var transformer = this, source;
-        do {
-            if (transformer[key] && transformer[key] !== DataSourceBase.prototype[key]) {
-                return;
-            }
-            source = transformer;
-            transformer = transformer.next;
-        } while (transformer);
-
-        return source;
-    },
-
-    /**
      * @implements dataModelAPI#install
      * @see {@link https://fin-hypergrid.github.io/core/doc/dataModelAPI.html#install|install}
      */
-    install: function(api, install) {
+    install: function(api, options) {
         var dataModel = this,
-            keys = getFilteredKeys(api = api || this);
+            keys = getFilteredKeys(api);
+
+        options = options || {};
 
         keys.forEach(function(key) {
-            if (install) {
-                var source = dataModel.needs.call(dataModel, key);
+            if (options.inject && !Array.isArray(api)) {
+                var source = needs(dataModel, key, options.force);
                 if (source) {
                     source[key] = api[key];
                 }
@@ -69,7 +57,25 @@ DataSourceBase.prototype = {
                 };
             }
         });
+    },
 
+    dispatchEvent: function(name, detail) {
+        this.handlers.forEach(function(handler) {
+            handler.call(this, name, detail);
+        }, this);
+    },
+
+    addListener: function(handler) {
+        if (this.handlers.indexOf(handler) < 0) {
+            this.handlers.push(handler);
+        }
+    },
+
+    removeListener: function(handler) {
+        var index = this.handlers.indexOf(handler);
+        if (index) {
+            delete this.handlers[index];
+        }
     },
 
 
@@ -108,26 +114,61 @@ DataSourceBase.prototype = {
     }
 };
 
-// api can be array or object
+/**
+ * Searches linked list of objects for implementation of `key` anywhere on their prototype chain.
+ * The search excludes members of `DataSourceBase.prototype`.
+ * @param {object} transformer - Data model transformer list, linked one to the next by `next` property. Falsy `next` means end-of-list.
+ * @param {string} key - Property to search for.
+ * @param {boolean} force - Always needed, so always delete other implementations (all implementations found along prototype chains of all transformers) and return last object in list.
+ * @returns {undefined|object} - `undefined` means an implementation was found; otherwise returns last object in list.
+ */
+function needs(transformer, key, force) {
+    var source;
+
+    do {
+        if (transformer[key]) {
+            if (force) {
+                for (var link = transformer; link && link !== Object.prototype; link = Object.getPrototypeOf(link)) {
+                    delete link[key];
+                }
+            } else if (transformer[key] !== DataSourceBase.prototype[key]) {
+                return; // means implementation exists (ignoring previously installed forwwarding catchers in base)
+            }
+        }
+        source = transformer;
+        transformer = transformer.next;
+    } while (transformer);
+
+    return source;
+}
+
+var blacklistAlways = ['constructor', 'initialize', '!keys', '!!keys'];
+
+/**
+ * The following keys (array elements or object keys) are filtered out:
+ * * Defined as something other than a function, including an accessor (getter and/or setter)
+ * * Keys missing from whitelist (not listed in string array `api['!!keys']`, when defined)
+ * * Keys blacklisted (listed in string array `api['!keys']` or `blacklistAlways`)
+ * @param {string[]|object} api
+ * @returns {string[]}
+ */
 function getFilteredKeys(api) {
     var whitelist = api.hasOwnProperty('!!keys') && api['!!keys'],
-        blacklist = api.hasOwnProperty('!keys') && api['!keys'],
-        keys = Array.isArray(api) ? api : Object.keys(api).filter(function(key) {
-            return typeof api[key] === 'function';
+        blacklist = blacklistAlways.concat(api.hasOwnProperty('!keys') && api['!keys'] || []),
+        keys;
+
+    if (Array.isArray(api)) {
+        keys = api;
+    } else {
+        keys = Object.keys(api).filter(function(key) {
+            return typeof Object.getOwnPropertyDescriptor(api, key).value === 'function';
         });
+    }
 
     return keys.filter(function(key) {
-        switch (key) {
-            case 'initialize':
-            case 'constructor':
-            case '!!keys':
-            case '!keys':
-                return;
-        }
-
         return !(
             whitelist && whitelist.indexOf(key) < 0 ||
-            blacklist && blacklist.indexOf(key) >= 0
+            blacklist.indexOf(key) >= 0
         );
     });
 }
@@ -139,7 +180,7 @@ function DataSourceError(message) {
     this.message = message;
 }
 
-// extend from `Error`
+// extend from `Error'
 DataSourceError.prototype = Object.create(Error.prototype);
 
 // override error name displayed in console
